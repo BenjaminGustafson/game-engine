118edf384d59ded805297bb6b3997218d79adece
BenjaminGustafson <bengustafson2@gmail.com>
2025-07-15 13:45:27 -0500

    vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,55 @@ public:
 	}
 private:
 	GLFWwindow* window;
+	VkInstance instance;
+
 	void initWindow() {
 		glfwInit();
 
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		// Don't alow window to resize to avoid issues
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
 		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Window", nullptr, nullptr);
 	}
+
 	void initVulkan() {
+		createInstance();
 	}
 
+	void createInstance() {
+		// Optional data given to the driver for optimization
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		// Tells the driver which global extensions and validation layers to use
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		createInfo.enabledLayerCount = 0;
+
+		// Create the instance using the structs made above, put it in &instance
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+		// Check that the instance was created 
+		if (result != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
 
 	void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
@@ -36,6 +74,7 @@ private:
 	}
 
 	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
 		glfwDestroyWindow(window);
 		glfwTerminate();
 	}
